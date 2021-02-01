b7d74777e34aaa8511238b17be5030afd08be283
saksham2007 <70582707+saksham2007@users.noreply.github.com>
2021-02-01 18:53:10 +0530

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,22 @@
+class Hero{
+    constructor(x,y,r)
+    {
+        var options={
+            density:1,
+            frictionAir:1
+          
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+        this.image=loadImage("images/Superhero-01.png")
+        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        World.add(world,this.body)
+    }
+    display(){
+        imageMode(CENTER)
+        image(this.image,this.x,this.y,this.r*2,this.r)
+        
+    }
+}
+
---
+++ monster.js
@@ -0,0 +1,21 @@
+class Monster{
+    constructor(x,y,r)
+    {
+        var options={
+            density:1,
+            frictionAir:1
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+        this.image=loadImage("images/Monster-01.png")
+        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        World.add(world,this.body)
+    }
+    display(){
+        imageMode(CENTER)
+        image(this.image,this.x,this.y,this.r*2,this.r*2)
+        
+    }
+}
+
