d5bc174504516b4ee284daa88c0361f65bed9f5c
saksham2007 <70582707+saksham2007@users.noreply.github.com>
2021-02-10 11:14:49 +0530

    Update monster.js

--- monster.js
+++ monster.js
@@ -3,19 +3,21 @@ class Monster{
     {
         var options={
             density:1,
-            frictionAir:1
+           // frictionAir:1
         }
         this.x=x
         this.y=y
         this.r=r
         this.image=loadImage("images/Monster-01.png")
-        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        this.body=Bodies.circle(this.x,this.y,(this.r)/2,options)
         World.add(world,this.body)
     }
     display(){
+        push()
+        var pos = this.body.position
         imageMode(CENTER)
-        image(this.image,this.x,this.y,this.r*2,this.r*2)
-        
+        image(this.image,pos.x,pos.y,this.r,this.r)
+        pop()
     }
 }
 
